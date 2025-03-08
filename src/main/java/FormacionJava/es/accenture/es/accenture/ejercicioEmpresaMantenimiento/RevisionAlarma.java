aa91fa357c77f7bc6bb17514b8cc713e6c7928be
IsaRB <romerobellorini@gmail.com>
2025-03-08 17:42:52 +0100

    Added class RevisionAlarma

---
+++ src/main/java/FormacionJava/es/accenture/es/accenture/ejercicioEmpresaMantenimiento/RevisionAlarma.java
@@ -0,0 +1,44 @@
+package FormacionJava.es.accenture.es.accenture.ejercicioEmpresaMantenimiento;
+
+
+import java.text.DecimalFormat;
+
+public class RevisionAlarma extends Servicio {
+    private int numeroAlarmas;
+
+    public RevisionAlarma(String fechaInicio, String cliente, int numeroAlarmas) {
+        super("Revisor Especialista Contraincendios", fechaInicio, cliente);
+        this.numeroAlarmas = numeroAlarmas;
+    }
+
+    public int getNumeroAlarmas() {
+        return numeroAlarmas;
+    }
+
+    public void setNumeroAlarmas(int numeroAlarmas) {
+        this.numeroAlarmas = numeroAlarmas;
+    }
+
+    @Override
+    public double costeMaterial() {
+        return 0;
+    }
+
+    @Override
+    public double costeManoObra() {
+        return (numeroAlarmas / 3.0) * 40;
+    }
+
+    @Override
+    public double costeTotal() {
+        return costeManoObra();
+    }
+
+    @Override
+    public String detalleServicio() {
+        DecimalFormat formato = new DecimalFormat("#.00");
+        return "Revisión de alarmas realizada para " + getCliente() +
+                " con un costo total de " + formato.format(costeTotal()) + "€";
+    }
+}
+
--- src/main/java/FormacionJava/es/accenture/es/accenture/ejercicioEmpresaMantenimiento/TrabajoPintura.java
+++ src/main/java/FormacionJava/es/accenture/es/accenture/ejercicioEmpresaMantenimiento/TrabajoPintura.java
@@ -1,5 +1,7 @@
 package FormacionJava.es.accenture.es.accenture.ejercicioEmpresaMantenimiento;
 
+import java.text.DecimalFormat;
+
 public class TrabajoPintura extends Servicio {
     private double superficie;
     private double precioPintura;
@@ -26,21 +28,28 @@ public class TrabajoPintura extends Servicio {
     //Métodos sobreescritos heredados de la clase Servicio
     @Override
     public double costeMaterial() {
-        return 0;
+        return (superficie / 7.8) * precioPintura;
     }
 
     @Override
     public double costeManoObra() {
-        return 0;
+        return (superficie / 10) * 9.5;
     }
 
     @Override
     public double costeTotal() {
-        return 0;
+        double total = costeMaterial() + costeManoObra();
+        if (superficie < 50) {
+            total *= 1.15; // Aumento del 15%
+        }
+        return total;
     }
 
     @Override
     public String detalleServicio() {
-        return "";
+        DecimalFormat formato = new DecimalFormat("#.00");
+        return "Trabajo de pintura realizado por " + getTrabajador() +
+                " para el cliente " + getCliente() +
+                " con un costo total de " + formato.format(costeTotal()) + "€";
     }
 }
