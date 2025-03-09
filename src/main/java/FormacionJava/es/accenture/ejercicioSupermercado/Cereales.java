2a33f917e34638ee1bd5aaa11dc6d23f2657c8f5
IsaRB <romerobellorini@gmail.com>
2025-03-09 12:18:46 +0100

    Add new changes in code Cereales

--- src/main/java/FormacionJava/es/accenture/ejercicioSupermercado/Cereales.java
+++ src/main/java/FormacionJava/es/accenture/ejercicioSupermercado/Cereales.java
@@ -4,46 +4,28 @@ import java.time.LocalDate;
 
 public class Cereales implements EsAlimento {
     private String marca;
-    private double precio;
     private String tipoCereal;
+    private double precio;
     private LocalDate caducidad;
 
-    public Cereales(String crunchy, String espelta, double v, LocalDate caducidad){}
+    public Cereales() {
+    }
 
-    public Cereales(String marca, double precio, String tipoCereal, LocalDate caducidad){
+    public Cereales(String marca, String tipoCereal, double precio, LocalDate caducidad) {
         this.marca = marca;
-        this.precio = precio;
         this.tipoCereal = tipoCereal;
+        this.precio = precio;
         this.caducidad = caducidad;
     }
 
-    //Getters y setters
-    public String getMarca() { return marca; }
-    public void setMarca(String marca) { this.marca = marca;}
-
-    public double getPrecio() { return precio; }
-    public void setPrecio(double precio) { this.precio = precio; }
-
-    public String getTipoCereal() { return tipoCereal; }
-    public void setTipoCereal(String tipoCereal) { this.tipoCereal = tipoCereal; }
-
-    //Métodos sobreescritos de EsAlimento
-    @Override
-    public void setCaducidad(LocalDate fc) {}
-    @Override
-    public LocalDate getCaducidad() { return caducidad;}
-
-    @Override
-    public String toString() {
-        return "Cereales{" +
-                "marca='" + marca + '\'' +
-                ", precio=" + precio +
-                ", tipoCereal='" + tipoCereal + '\'' +
-                ", caducidad=" + caducidad +
-                '}';
+    public void setCaducidad(LocalDate fc) {
+        this.caducidad = fc;
     }
 
-    @Override
+    public LocalDate getCaducidad() {
+        return caducidad;
+    }
+
     public int getCalorias() {
         switch (tipoCereal.toLowerCase()) {
             case "espelta":
@@ -55,6 +37,12 @@ public class Cereales implements EsAlimento {
             default:
                 return 15;
         }
+    }
 
+    @Override
+    public String toString() {
+        return "Cereales(marca=" + marca + ", tipoCereal=" + tipoCereal +
+                ", precio=" + precio + ", caducidad=" + caducidad +
+                ", calorías=" + getCalorias() + ")";
     }
 }
